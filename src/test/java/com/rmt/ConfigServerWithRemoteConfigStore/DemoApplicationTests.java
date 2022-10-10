08f3b9ab1a22a38e053a7d583787758fd9ed04c9
rajendra <rtodkari@gsngames.com>
2022-10-10 09:43:46 +0530

    application.yml - reading config from git hub repo

---
+++ src/test/java/com/rmt/ConfigServerWithRemoteConfigStore/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmt.ConfigServerWithRemoteConfigStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
